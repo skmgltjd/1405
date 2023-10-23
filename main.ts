43442561961fff6cf042fe0e32f3425851c71784
skmgltjd <148729091+skmgltjd@users.noreply.github.com>
2023-10-23 09:23:57 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,23 @@
+basic.showLeds(`
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.showIcon(IconNames.Square)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
